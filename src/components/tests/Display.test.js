4fd8b73d86ddb70823ec1f413b1d7650f25fc5f6
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-01-24 20:11:15 -0500

    Finished the suite for Episode Test and Show Test

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import { render, screen, waitFor} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import Display from './../Display';
+import Display from "./../Display";
 
+const testShow = {
+  name: "Game Of Thrones",
+  summary: "Best show ever",
+  seasons: [
+    {
+      id: 0,
+      name: "Season 1",
+      episodes: [],
+    },
+    {
+      id: 1,
+      name: "Season 2",
+      episodes: [],
+    },
+    {
+      id: 2,
+      name: "Season 3",
+      episodes: [],
+    },
+  ],
+};
 
-test('renders without errors with no props', ()=>{
-
+test("renders without errors with no props", () => {
+  render(<Display />);
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test("renders Show component when the button is clicked ", () => {
+  render(<Display />);
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+});
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+test("renders show season options matching your data when the button is clicked", () => {});
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import { render, screen } from '@testing-library/react';
-import Episode from './../Episode';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import Episode from "./../Episode";
 
+const testEpisode = {
+  id: 1,
+  image: "https://i.ibb.co/2FsfXqM/stranger-things.png",
+  name: "",
+  season: 1,
+  number: 1,
+  summary: "summary",
+  runtime: 1,
+};
+const testEpisodeImage = {
+  id: 1,
+  image: null,
+  name: "",
+  season: 1,
+  number: 1,
+  summary: "summary",
+  runtime: 1,
+};
 
-test("renders without error", () => {});
+test("renders without error", () => {
+  render(<Episode episode={testEpisode} />);
+});
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders the summary test passed as prop", () => {
+  render(<Episode episode={testEpisode} />);
 
-test("renders default image when image is not defined", ()=>{});
+  const summary = screen.queryByText(/summary/i);
+  expect(summary).toBeInTheDocument();
+  expect(summary).toBeVisible();
+  expect(summary).toHaveTextContent("summary");
+});
+
+test("renders default image when image is not defined", () => {
+  render(<Episode episode={testEpisodeImage} />);
+
+  const image = screen.queryByAltText(
+    "https://i.ibb.co/2FsfXqM/stranger-things.png"
+  );
+  expect(image).toBeVisible();
+});
